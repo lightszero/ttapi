27e10312958d967faeaf321a87b4a27bb6fc60cb
lights <lightsever@hotmail.com>
2024-10-29 22:23:37 +0800

    transform fallback 粒子在手机上表现不对

--- wegame/game.ts
+++ wegame/game.ts
@@ -4,6 +4,7 @@
 import { tt_impl } from "./ttimpl_wegame/ttimpl_wx.js"
 import { GameApp } from "./ttlayer2/ttlayer2.js"
 import { TTState_Draw } from "./ttsample/ttstate_draw.js";
+import { TTState_Scene } from "./ttsample/ttstate_scene.js";
 console.log("-->这是mini game<--");
 
 //var info = wx.loadFont("./VonwaonBitmap-16px.ttf")
@@ -13,5 +14,5 @@ let impl = new tt_impl.ttimpl_wx();
 impl.Init();
 
 //初始化layer2
-GameApp.Start(new TTState_Draw());
+GameApp.Start(new TTState_Scene());
 
