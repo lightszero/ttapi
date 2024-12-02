0a9b841e6e2f70542c127aef66788a8a0e3bdad1
lights <lightsever@hotmail.com>
2024-12-02 21:10:06 +0800

    修复elem 替换sprite uv bug

--- common/ttlayer2/resources/atlas/namedelem.ts
+++ common/ttlayer2/resources/atlas/namedelem.ts
@@ -102,7 +102,7 @@ export class NamedElementPacked {
 
         e.sizeTL = new Vector2(s.pixelwidth / -2, s.pixelheight / -2);
         e.sizeRB = new Vector2(s.pixelwidth / 2, s.pixelheight / 2);
-        e.uvCenter = new Vector2(s.uv.U1 * 0.5 + s.uv.U2 * 0.5, s.uv.V1 * 0.5 + s.uv.V2);
+        e.uvCenter = new Vector2(s.uv.U1 * 0.5 + s.uv.U2 * 0.5, s.uv.V1 * 0.5 + s.uv.V2*0.5);
         e.uvHalfSize = new Vector2((s.uv.U2 - s.uv.U1) * 0.5, (s.uv.V2 - s.uv.V1) * 0.5);
         e.uvLayer = s.uvlayer;
         e.eff = eff;
